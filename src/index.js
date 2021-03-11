07b5087048b27f4725406752ccaa417c9763e0bc
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-03-12 01:50:36 +0200

    Сделал темы и рендер

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import menu from'./menu.json';
+import menuItems from './menu-items.hbs';
 import './styles.css';
+
+const menuList = document.querySelector('.js-menu');
+const switcher = document.querySelector('#theme-switch-toggle');
+const cardMarkup = menuItems(menu);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+menuList.insertAdjacentHTML('afterbegin', cardMarkup);
+
+if (!localStorage.theme) {
+  document.body.classList.add(Theme.LIGHT);
+} else {
+  document.body.classList.add(localStorage.theme);
+  if (document.body.classList.contains(Theme.DARK)) {
+    switcher.checked = 'true';
+  }
+}
+
+switcher.addEventListener('change', () => {
+  document.body.classList.toggle(Theme.DARK);
+  document.body.classList.toggle(Theme.LIGHT);
+  localStorage.theme = document.body.classList.contains(Theme.DARK) ? Theme.DARK : Theme.LIGHT;
+})
